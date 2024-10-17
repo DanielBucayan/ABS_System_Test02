727823ae6aefcfc6d8a3a9ebb920fd0999efafba
Daniel <122674836+DanielBucayan@users.noreply.github.com>
2024-10-17 08:12:07 +0000

    updated urls.py

--- django_auth/django_project/urls.py
+++ django_auth/django_project/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # new
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("accounts/", include("allauth.urls")),  # new
